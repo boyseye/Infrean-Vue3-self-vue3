f1edcd44b531e062cf170019b4f2c058024d57f1
boyseye <boyseye29@gmail.com>
2023-11-26 10:36:12 +0900

    20231125 eslint prettier code on save formatter

--- vue-project/.eslintrc.cjs
+++ vue-project/.eslintrc.cjs
@@ -3,12 +3,23 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    {
+      "prettier/prettier": [
+        "error",
+        {
+          tabWidth:2,
+          singleQuote: true,
+          useTabs : true
+        },
+
+      ]
+    }
   }
 }
